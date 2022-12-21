6e12efaaaa2c112a927a994d5d7ef2680c62c363
Korozin <90534409+Korozin@users.noreply.github.com>
2022-12-21 09:02:55 -0500

    Remove from ZIP -- Add Files

---
+++ src/entry.c
@@ -0,0 +1,31 @@
+#include "tcpgecko/main.h"
+#include "tcpgecko/title.h"
+#include "dynamic_libs/socket_functions.h"
+#include "dynamic_libs/gx2_functions.h"
+#include "utils/logger.h"
+#include "tcpgecko/tcp_gecko.h"
+#include "common/common.h"
+
+int entry() {
+	if (isRunningAllowedTitleID()) {
+		InitOSFunctionPointers();
+		InitSocketFunctionPointers();
+		InitGX2FunctionPointers();
+
+		log_init(COMPUTER_IP_ADDRESS);
+		log_printf("\n\n\n==================\nWiiUHackingElf by cedkeChat123\n");
+	    log_printf("starting...\n\n");
+		startTCPGecko();
+
+		return EXIT_RELAUNCH_ON_LOAD;
+	}
+
+	//! *******************************************************************
+	//! *                 Jump to our application                    *
+	//! *******************************************************************
+	return Menu_Main();
+}
+
+int __entry_menu(int argc, char **argv) {
+	return entry();
+}
